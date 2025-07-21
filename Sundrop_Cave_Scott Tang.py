ae8e8cdcb0d7af7bb0ab9bac1655641a09bd41e0
baddestpiggy <scottcjtang@gmail.com>
2025-07-21 17:40:02 +0800

    started working on the game, added main menu and town choices

--- Sundrop_Cave_Scott Tang.py
+++ Sundrop_Cave_Scott Tang.py
@@ -12,7 +12,7 @@ WIN_GP = 500
 
 minerals = ['copper', 'silver', 'gold']
 mineral_names = {'C': 'copper', 'S': 'silver', 'G': 'gold'}
-pickaxe_price = [50, 150]
+pickaxe_price = [50, 150] # 50 is copper, 150 is gold
 
 prices = {}
 prices['copper'] = (1, 3)
@@ -43,7 +43,8 @@ def load_map(filename, map_struct):
 def clear_fog(fog:list, player: dict):
     plr_x = player.get("x")
     plr_y = player.get("y")
-    
+    if plr_x == None or plr_y == None:
+        return
     #3x3 square around player coordinates is -1,-1
     #need check if clearing fog is outside of map range also
     for x in range(-1,2,1):
@@ -78,12 +79,12 @@ def initialize_game(game_map:list, fog: list, player:dict):
     player['silver'] = 0
     player['gold'] = 0
     player['GP'] = 0
-    player['day'] = 0
+    player['day'] = 1
     player['steps'] = 0
     player['turns'] = TURNS_PER_DAY
-
+    player['name'] = ''
     player['backpackslots'] = 10
-
+    player['pickaxelevel'] = 1
     clear_fog(fog, player)
     return game_map
     
@@ -102,17 +103,56 @@ def draw_map(game_map:list, fog:list, player:dict):
             element = row[elementindex]
             if fog[rowindex][elementindex] == "?":
                 element = "?"
+            elif rowindex == plr_y and elementindex == plr_x:
+                element = "M"
             print(element,end = '')
         print("|")
     print(horizontalborder)
     return
 
 # This function draws the 3x3 viewport
-def draw_view(game_map, fog, player):
+def draw_view(game_map:list, fog:list, player:dict):
+    plr_x = player.get("x")
+    plr_y = player.get("y")
+    if plr_x == None or plr_y == None:
+        return
+    #print border
+    horizontalborder = "+"+"-"*3+"+"
+    print(horizontalborder)
+    
+    for x in range(-1,2,1):
+        print("|",end = '')
+        for y in range(-1,2,1):
+            
+            if (plr_y + y < 0 or plr_x + x < 0) or (plr_y + y > len(game_map) or plr_x + x > len(game_map)): #checks if player is on borders
+                print("#",end = '')
+            else:
+                print(game_map[y][x],end = '')
+        print("|")
+    print(horizontalborder)
     return
 
+def buy_stuff(player:dict):
+    backpackslots = player['backpackslots']
+    print("----------------------- Shop Menu -------------------------")
+    print("(B)ackpack upgrade to carry {} items for {} GP".format(backpackslots+2,backpackslots*2))
+    print("(L)eave shop")
+    print("-----------------------------------------------------------")
+    print("GP: {}")
+    print("-----------------------------------------------------------")
+    return
 # This function shows the information for the player
 def show_information(player):
+    print("----- Player Information -----")
+    print("Name: {}")
+    print("Portal position: {}")
+    print("Pickaxe level: {} {}")
+    print("------------------------------")
+    print("GP: {}")
+    print("Steps taken: {}")
+    print("------------------------------")
+
+
     return
 
 # This function saves the game
@@ -139,7 +179,7 @@ def show_main_menu():
     print("------------------")
 
 def show_town_menu(day):
-    print("DAY {}".format(str(day)))
+    print("\nDAY {}".format(str(day)))
     # TODO: Show Day
     print("----- Sundrop Town -----")
     print("(B)uy stuff")
@@ -149,8 +189,56 @@ def show_town_menu(day):
     print("Sa(V)e game")
     print("(Q)uit to main menu")
     print("------------------------")
-            
+       
+def main_menu(game_map,fog,player):
+    show_main_menu()
+    menuchoice = input("Your choice? ")
+    if menuchoice.lower() == "n":
+        game_map = initialize_game(game_map,fog,player)
 
+        name = input("Greetings, miner! What is your name? ")
+        print("Pleased to meet you, {}. Welcome to Sundrop Town!".format(name))
+
+        player['name'] = name
+
+        return "",game_map
+    elif menuchoice.lower() == "l":
+        
+        return
+    elif menuchoice.lower() == "q":
+        
+        return 'Exit',[]
+
+def town_menu_actions(game_map,fog,player:dict):
+    
+    while True:
+
+        show_town_menu(player["day"])
+        townchoice = input("Your choice? ")
+        if townchoice.lower() == "b": #TODO: next time make option to upgrade pickaxe
+            while True:
+                buy_stuff(player)
+                buy_choice = input("Your choice? ")
+                if buy_choice.lower() == "b":
+                    if player["GP"] >= player["backpackslots"]:
+                        player["backpackslots"] += 2
+                        print("Congratulations! You can now carry {} items!".format(player["backpackslots"]))
+                        continue
+                elif buy_choice.lower() == 'q':
+                    break
+            
+        elif townchoice.lower() == "i":
+            show_information(player)
+        elif townchoice.lower() == "m":
+            draw_map(game_map,fog,player)
+
+        elif townchoice.lower() == "e":
+            return "Enter"
+        elif townchoice.lower() == "v":
+            print('saving tha game')
+        elif townchoice.lower() == "q":
+            return "Exit"
+    
 #--------------------------- MAIN GAME ---------------------------
 game_state = 'main'
 print("---------------- Welcome to Sundrop Caves! ----------------")
@@ -163,37 +251,19 @@ print("-----------------------------------------------------------")
 
 # TODO: The game!
 
-game_map = initialize_game(game_map,fog,player)
-print(player)
 
-'''while True:
 
-    show_main_menu()
-    menuchoice = input("Your choice? ")
-    if menuchoice.lower() == "n":
-        #day = 1
-        #backpackslots = 10
 
-        player = {}
-        initialize_game()
 
-        name = input("Greetings, miner! What is your name? ")
-        print("Pleased to meet you, {}. Welcome to Sundrop Town!\n".format(name))
-        print(show_town_menu(day))
-        townchoice = input("Your choice? ")
-        if townchoice.lower() == "b":
-    
-        elif townchoice.lower() == "i":
-
-        elif townchoice.lower() == "m":
-
-        elif townchoice.lower() == "e":
-
-        elif townchoice.lower() == "v":
-            
-        elif townchoice.lower() == "q":
+while True:
+    main_choice,game_map = main_menu(game_map,fog,player)
+    print(game_map)
+    if main_choice == "Exit":
+        break
+    else:
+        town_action = town_menu_actions(game_map,fog,player)
+        if town_action == "Exit":
             continue
-    elif menuchoice.lower() == "l":
+        elif town_action == "Enter":
+            print("Entering tha mine")
         
-    elif menuchoice.lower() == "q":
-        break'''
